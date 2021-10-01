cbc1c22ff0b24fe28c9415950fd224e505d85bf6
Jesutooni <edeworuyoyou@gmail.com>
2021-10-01 09:12:53 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="flex container h-screen w-full">
+
+    <!-- Side Nav -->
+    <div class="w-1/5 border-r border-lighter px-8 py-2">
+      <button class=""></button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        blue: {
+          100: 'EFF9FF',
+          200: '#1DA1F2',
+          300: '#2795D9'
+        },
+        cyan: {
+          100: '#F5f8FA',
+          200: '#E1E8ED',
+          300: '#AAB8C2',
+          400: '#657786'
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
